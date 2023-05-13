19befd76a02f800e65ed016bc70051afdf3c94a3
wisamosman <viesam_1987@hotmail.com>
2023-05-13 15:31:55 +0200

    add venv

--- dgbolg/src/posts/models.py
+++ dgbolg/src/posts/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 class post(models.Model):
     title = models.CharField(max_length=100)
+    publish_date = models.DateTimeField()
+    content = models.TextField(max_length=15000)
