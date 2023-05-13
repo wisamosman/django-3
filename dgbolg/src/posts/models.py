44035536a7e95c24252f3b9cee10b056a9f6a925
wisamosman <viesam_1987@hotmail.com>
2023-05-13 23:39:24 +0200

    add venv

--- dgbolg/src/posts/models.py
+++ dgbolg/src/posts/models.py
@@ -14,3 +14,4 @@ class post(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
+    author = models.ForeignKey()
