ea6959113e945ee1997331f0d1132c4596314ab0
wisamosman <viesam_1987@hotmail.com>
2023-05-13 23:29:04 +0200

    add venv

--- dgbolg/src/posts/models.py
+++ dgbolg/src/posts/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class author (models.Model):
+    name = models.CharField(max_length=1000)
+    bio = models.TextField(max_length=500)
+
+
+
+
+
 class post(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateTimeField()
