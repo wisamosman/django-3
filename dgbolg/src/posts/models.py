baee7ca3056421512ad683b2c3b8dcf912153403
wisamosman <viesam_1987@hotmail.com>
2023-05-14 00:11:32 +0200

    add venv

--- dgbolg/src/posts/models.py
+++ dgbolg/src/posts/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class author (models.Model):
     name = models.CharField(max_length=1000)
     bio = models.TextField(max_length=500)
+    def __str__(self):
+        return self.name
+        
 
 
 
@@ -15,3 +18,6 @@ class post(models.Model):
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
     author = models.ForeignKey(author,related_name='post_author', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+        
