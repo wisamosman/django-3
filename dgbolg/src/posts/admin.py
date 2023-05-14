913fc8014e9f39ef9d510218df8124418a24acb2
wisamosman <viesam_1987@hotmail.com>
2023-05-14 11:30:57 +0200

    add venv

--- dgbolg/src/posts/admin.py
+++ dgbolg/src/posts/admin.py
@@ -6,6 +6,7 @@ from .models import post, author
 class postAdmin(admin.ModelAdmin):
     list_display = ['title' ,'author', 'publish_date']
     list_filter = ['author','tags','publish_date']
+    search_fields = ['title','content']
 
 admin.site.register(post,postAdmin)
 admin.site.register(author)
