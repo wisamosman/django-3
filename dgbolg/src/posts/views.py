3ba8ff93f3f0cbee8e82b7c2b76d6c93cb2de574
wisamosman <viesam_1987@hotmail.com>
2023-05-18 16:56:13 +0200

    add venv

--- dgbolg/src/posts/views.py
+++ dgbolg/src/posts/views.py
@@ -4,3 +4,7 @@ from.models import post
 def post_list(request):
     date = post.objects.all()
     return render(request, 'posts/posts.html',{'posts':date})
+
+def post_detail(request,post_id):
+    date = post.objects.get(id=post_id)
+    return render(request, 'posts/detail.html',{'post':date})
